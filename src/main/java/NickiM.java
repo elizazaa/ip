deb52981f00b32cc1d117cc65c488687191d602b
Eliza Cajetas <elizacajetas@u.nus.edu>
2025-01-26 04:08:55 +0800

    changed what duke class prints out in the main method renamed the class and the file NickiM

---
+++ src/main/java/NickiM.java
@@ -0,0 +1,7 @@
+public class NickiM {
+    public static void main(String[] args) {
+        System.out.println("Hello! I'm NickiM");
+        System.out.println("What can I do for you?");
+        System.out.println("Bye. Hope to see you again soon!");
+    }
+}
