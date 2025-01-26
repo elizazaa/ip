f1536e864a06523af9202ffe59df163ae992b81b
Eliza Cajetas <elizacajetas@u.nus.edu>
2025-01-26 17:51:28 +0800

    added functions and constructor into task class

--- src/main/java/Task.java
+++ src/main/java/Task.java
@@ -1,3 +1,25 @@
 public class Task {
+    private String toDo;
+    private boolean isDone;
+    public Task(String description) {
+        this.toDo = description;
+        this.isDone = false;
+    }
+
+    public void mark() {
+        isDone = true;
+    }
+
+    public void unMark() {
+        isDone = false;
+    }
+
+    public String getStatusIcon() {
+        return (isDone ? "X" : " "); // X for done, blank for not done
+    }
     
+    @Override
+    public String toString() {
+        return "[" + getStatusIcon() + "] " + toDo;
+    }
 }
