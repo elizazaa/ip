d0279324c708b16d279208c94690292c3f3e473b
Eliza Cajetas <elizacajetas@u.nus.edu>
2025-01-26 17:32:34 +0800

    imported ArrayList and List to use for making the toDo list

--- src/main/java/NickiMinaj.java
+++ src/main/java/NickiMinaj.java
@@ -1,19 +1,36 @@
 import java.util.Scanner;
+import java.util.List;
+import java.util.ArrayList;
 public class NickiMinaj {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         System.out.println("Hello! I'm NickiMinaj \n" +
                 "I am not Jasmine, I am Aladdin");
-        System.out.println("Are these the things that I get for pulling you students on?");
+        System.out.println("Watchu wanna do, Barb?");
 
         while (true) {
             String input = scanner.nextLine();
+
+            List<String> toDo = new ArrayList<>();
+            toDo.add(input);
+
             if (input.equals("bye")) {
-                System.out.println("gurl bye");
+                System.out.println("Imma run away Imma run away aye");
                 break;
+            } else if (input.equals("list")) {
+                for (String task : toDo) {
+                    System.out.println(task);
+                }
+            } else {
+                System.out.println("added:" + input);
             }
-            System.out.println(input);
         }
         scanner.close();
     }
+
+    /*public static String listToString(List<String> list) {
+        for (String task : list) {
+
+        }
+    }*/
 }
