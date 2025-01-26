225e81755c44de1a002251b38a1d13de38504e9a
Eliza Cajetas <elizacajetas@u.nus.edu>
2025-01-26 17:11:22 +0800

    Echoes inputs using scanner import

--- src/main/java/NickiMinaj.java
+++ src/main/java/NickiMinaj.java
@@ -1,7 +1,19 @@
+import java.util.Scanner;
 public class NickiMinaj {
     public static void main(String[] args) {
-        System.out.println("Hello! I'm NickiMinaj");
-        System.out.println("What can I do for you?");
-        System.out.println("Bye. Hope to see you again soon!");
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Hello! I'm NickiMinaj \n" +
+                "I am not Jasmine, I am Aladdin");
+        System.out.println("Are these the things that I get for pulling you students on?");
+
+        while (true) {
+            String input = scanner.nextLine();
+            if (input.equals("bye")) {
+                System.out.println("gurl bye");
+                break;
+            }
+            System.out.println(input);
+        }
+        scanner.close();
     }
 }
