1ef3ff969cc906ad6c8bf50e02d95882e69ea9a8
Eliza Cajetas <elizacajetas@u.nus.edu>
2025-01-28 00:09:52 +0800

    feat: Enable event class to use LocalDateTime using LocalTimeFormatter

--- src/main/java/Event.java
+++ src/main/java/Event.java
@@ -2,28 +2,37 @@ import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
 
 public class Event extends Task {
-    private final LocalDateTime by;
-    protected String from;
-    protected String to;
+    protected LocalDateTime from;
+    protected LocalDateTime to;
+
     public Event(String description, String from, String to) {
         super(description);
-        this.from = from;
-        this.to = to;
+        this.from = parseDateTime(from);
+        this.to = parseDateTime(to);
     }
 
-    public Event(String description, String from, String to, boolean isDone) {
+    public Event(String description, LocalDateTime from, LocalDateTime to, boolean isDone) {
         super(description, isDone);
         this.from = from;
         this.to = to;
     }
-    
+
+    private LocalDateTime parseDateTime(String dateTime) {
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("d/M/yyyy HHmm");
+        return LocalDateTime.parse(dateTime, formatter);
+    }
+
     @Override
     public String serialize() {
-        return "E | " + (isDone ? "1" : "0") + " | " + description + " | " + from + " | " + to;
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("d/M/yyyy HHmm");
+        return "E | " + (isDone ? "1" : "0") + " | " + description + " | " + from.format(formatter) + " | " + to.format(formatter);
     }
 
     @Override
     public String toString() {
-        return "[E]" + super.toString() + " (from: " + from + " to: " + to + ")";
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("d MMM yyyy, h:mma");
+        return "[E][" + (isDone ? "✓" : "✗") + "] " + description
+                + " (from: " + from.format(formatter)
+                + " to: " + to.format(formatter) + ")";
     }
 }
