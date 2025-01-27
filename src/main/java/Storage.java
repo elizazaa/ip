07b0360566b0c38aeba26a6aaa2de0de2d1c2c99
Eliza Cajetas <elizacajetas@u.nus.edu>
2025-01-27 18:46:16 +0800

    Update storage.java

---
+++ src/main/java/Storage.java
@@ -0,0 +1,55 @@
+import java.io.*;
+import java.sql.Array;
+import java.util.ArrayList;
+import java.util.List;
+
+public class Storage {
+    private final String filePath; //stores the path to the file where tasks will be saved
+
+    public Storage(String filePath) {
+        this.filePath = filePath;
+    }
+
+    public List<Task> loadTasks() {
+        List<Task> tasks = new ArrayList<>();
+        File file = new File(filePath);
+        if (!file.exists()) {
+            //create the file and directories if they dont exist
+            try {
+                file.getParentFile().mkdirs();
+                file.createNewFile();
+            } catch (IOException e) {
+                System.out.println("Error creating file: " + e.getMessage());
+            }
+        } else {
+            try (BufferedReader reader = new BufferedReader(new FileReader(file))) {
+                String line;
+                while ((line = reader.readLine()) != null) {
+                    Task task = parseTask(line);
+                    if (task != null) {
+                        tasks.add(task);
+                    }
+                }
+            } catch (IOException e) {
+                System.out.println("Error reading file: " + e.getMessage());
+            }
+        }
+        return tasks;
+    }
+
+    private Task parseTask(String line) {
+        String[] parts = line.split(" \\| ");
+        String type = parts[0];
+        boolean isDone = parts[1].equals("1");
+        switch (type) {
+            case "T":
+                return new Todo(parts[2], isDone);
+            case "D":
+                return new Deadline(parts[2], parts[3], isDone);
+            case "E":
+                return new Event(parts[2], parts[3], parts[4], isDone);
+            default:
+                return null;
+        }
+
+}
