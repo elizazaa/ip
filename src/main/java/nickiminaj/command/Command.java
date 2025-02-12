9c6b366e4ad3cc9cc3d82cc5c6a67f39ba7aa1c6
Eliza Cajetas <elizacajetas@u.nus.edu>
2025-02-12 17:02:51 +0800

    Add JavaDoc to Command class and its subclasses

--- src/main/java/nickiminaj/command/Command.java
+++ src/main/java/nickiminaj/command/Command.java
@@ -5,8 +5,29 @@ import nickiminaj.Storage;
 import nickiminaj.TaskList;
 import nickiminaj.Ui;
 
+/**
+ * An abstract class representing a command that can be executed.
+ * Subclasses must implement the {@code execute} method to define
+ * specific command behaviors.
+ */
 public abstract class Command {
+
+    /**
+     * Executes the command, performing the necessary actions.
+     *
+     * @param tasks   The task list that may be modified by the command.
+     * @param ui      The user interface to display messages.
+     * @param storage The storage system to persist any necessary data.
+     * @throws Exception If an error occurs during execution.
+     */
     public abstract void execute(TaskList tasks, Ui ui, Storage storage) throws DukeException;
+
+    /**
+     * Determines whether this command signals the program to exit.
+     * By default, commands do not exit the program.
+     *
+     * @return {@code false} by default, overridden by exit commands.
+     */
     public boolean isExit() {
         return false;
     }
